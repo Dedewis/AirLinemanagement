04f232520624d8dd530c314ffcee000cb95620f0
Kavya M <149556067+Dedewis@users.noreply.github.com>
2024-09-27 19:37:52 +0530

    Add files via upload

---
+++ am/DriverManager.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package am;
+
+import com.sun.jdi.connect.spi.Connection;
+
+/**
+ *
+ * @author kavya
+ */
+class DriverManager {
+
+    static Connection getConnection(String jdbcmysqllocalhost3306ams, String root, String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ am/PreparedStatement.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package am;
+
+/**
+ *
+ * @author kavya
+ */
+class PreparedStatement {
+
+    void setString(int i, String text) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void executeUpdate() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
